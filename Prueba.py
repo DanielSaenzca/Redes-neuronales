604bbcd5e1bedca551fbc7ca97a62b6cfcafbb53
DanielSaenz <juan.saenzc@alumno.buap.mx>
2022-09-10 17:48:57 -0500

    Prueba2

--- Prueba.py
+++ Prueba.py
@@ -1,6 +1,6 @@
 import mnist_loader
 import network
-import pickle
+import pickle #Libreria para guardar cualquier objeto serializable de python en disco duro
 training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 test_data = list(test_data)
